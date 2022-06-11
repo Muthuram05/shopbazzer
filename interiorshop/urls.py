7b87f6e0d9e72b1c5e0e26975c3d0124945d612a
Muthu Raman <88555538+Muthuram05@users.noreply.github.com>
2022-06-12 01:24:24 +0700

    Update urls.py

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('cart/', include('apps.cart.urls')),
     path('', include('apps.core.urls')),
     path('', include('apps.product.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
